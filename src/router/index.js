316d7ae7f20e81231978bdab8bf6325ffb3f527c
Subramanyarao11 <subramanya11rao@gmail.com>
2023-01-28 17:44:41 +0530

    added login and signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
-import Login from  "../components/Login.vue"
+import Login from "../components/Login.vue"
+import Signup from "../components/Signup.vue"
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/signup',
+        name: 'signup',
+        component: Signup
+    },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
